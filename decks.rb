d5e1edb84556238858389e24da2c5cd1279a6e7f
Daryl Watson <darylwtsn@gmail.com>
2015-04-27 22:37:19 +0100

    Refactored decks.rb

--- decks.rb
+++ decks.rb
@@ -1,92 +1,19 @@
 # This program creates a deck of cards, stores it as an array
-# It then shuffles the deck and shows you the first five cards 
+# It then shuffles the deck and shows you the first five cards
 
+$deck = []
 
-deck = []
-
-suit = ""
-number = 0
-card = ""
-
-4.times do |x|
-	if x == 0
-		suit = "D"
-		14.times do |x|
-			if x == 1
-				number = "A"
-			elsif x == 11
-				number = "J"
-			elsif x == 12
-				number = "Q"
-			elsif x == 13
-				number = "K"
-			else 	
+def createdeck(suit)
+		for x in 2..10
 				number = x.to_s
-			end
-			if x != 0
-				card = suit + number
-				deck.push(card)
-			end
+				$deck.push(suit + number)
 		end
-	elsif x == 1
-		suit = "H"
-		14.times do |x|
-			if x == 1
-				number = "A"
-			elsif x == 11
-				number = "J"
-			elsif x == 12
-				number = "Q"
-			elsif x == 13
-				number = "K"
-			else 	
-				number = x.to_s
-			end
-			if x != 0
-				card = suit + number
-				deck.push(card)
-			end
-		end
-	elsif x == 2
-		suit = "C"
-		14.times do |x|
-			if x == 1
-				number = "A"
-			elsif x == 11
-				number = "J"
-			elsif x == 12
-				number = "Q"
-			elsif x == 13
-				number = "K"
-			else 	
-				number = x.to_s
-			end
-			if x != 0
-				card = suit + number
-				deck.push(card)
-			end
-		end
-	elsif x == 3
-		suit = "S"
-		14.times do |x|
-			if x == 1
-				number = "A"
-			elsif x == 11
-				number = "J"
-			elsif x == 12
-				number = "Q"
-			elsif x == 13
-				number = "K"
-			else 	
-				number = x.to_s
-			end
-			if x != 0
-				card = suit + number
-				deck.push(card)
-			end
-		end
-	end
+		faces = ["J","Q","K","A"]
+		faces.each {|x| $deck.push(suit + x)}
 end
 
-deck.shuffle!
-puts "These are your cards #{deck[0]}, #{deck[1]}, #{deck[2]}, #{deck[3]}, #{deck[4]}"
+suitarray = ["D", "H", "C", "S"]
+suitarray.each { |suit| createdeck(suit)}
+
+$deck.shuffle!
+puts "These are your cards #{$deck[0]}, #{$deck[1]}, #{$deck[2]}, #{$deck[3]}, #{$deck[4]}"
